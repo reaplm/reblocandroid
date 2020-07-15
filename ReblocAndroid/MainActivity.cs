ec5959ee527468543c66ce3204a0c7ab1685d605
PMolefe <PMolefe@G69BLAPICT80>
2020-07-15 14:45:53 +0200

    initial commit

---
+++ ReblocAndroid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.Design.Widget;
+using Android.Views;
+
+namespace ReblocAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        private DrawerLayout drawerLayout;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.drawer_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            //Setup Navigation Drawer 
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawer_close);
+            drawerLayout.AddDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+
+            //Setup Navigation View
+            SetNavigationViewListener();
+        }
+        private void SetNavigationViewListener()
+        {
+            NavigationView navigationView = drawerLayout.FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
