04b484384b280ac5934329992ee99800d29f5042
PMolefe <PMolefe@G69BLAPICT80>
2020-07-29 09:48:04 +0200

    register activity ui

--- ReblocAndroid/LoginActivity.cs
+++ ReblocAndroid/LoginActivity.cs
@@ -7,20 +7,49 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
 namespace ReblocAndroid
 {
     [Activity(Label = "LoginActivity")]
-    public class LoginActivity : Activity
+    public class LoginActivity : AppCompatActivity
     {
+        private EditText email;
+        private EditText password;
+        private TextView registerLink;
+        private Button loginButton;
+        private CheckBox rememberMeCheck;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.activity_login);
+            Title = "Login";
+
+            email = FindViewById<EditText>(Resource.Id.email);
+            password = FindViewById<EditText>(Resource.Id.password);
+            registerLink = FindViewById<TextView>(Resource.Id.register_link);
+            loginButton = FindViewById<Button>(Resource.Id.login_button);
+            rememberMeCheck = FindViewById<CheckBox>(Resource.Id.login_rememberme);
+
+            registerLink.Click += RegisterLink_Click;
+            loginButton.Click += LoginButton_Click;
+        }
+
+        private void RegisterLink_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(RegisterActivity));
+
+            StartActivity(intent);
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ ReblocAndroid/RegisterActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace ReblocAndroid
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : AppCompatActivity
+    {
+        private EditText fName;
+        private EditText lName;
+        private EditText phone;
+        private EditText email;
+        private EditText password;
+        private Button registerButton;
+        private CheckBox rememberMeCheck;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_register);
+
+            Title = "Register";
+
+            fName = FindViewById<EditText>(Resource.Id.register_fname);
+            lName = FindViewById<EditText>(Resource.Id.register_lname);
+            phone = FindViewById<EditText>(Resource.Id.register_phone);
+            email = FindViewById<EditText>(Resource.Id.register_email);
+            password = FindViewById<EditText>(Resource.Id.register_password);
+            phone = FindViewById<EditText>(Resource.Id.register_phone);
+            registerButton = FindViewById<Button>(Resource.Id.register_button);
+            rememberMeCheck = FindViewById<CheckBox>(Resource.Id.register_rememberme);
+
+            registerButton.Click += RegisterButton_Click;
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
